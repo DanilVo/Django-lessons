24af6ac38df0af6226caecd37ac34f0d09200fbe
DanilV <dani736@gmail.com>
2022-07-09 18:40:39 +0300

    lesson 7

--- djsite/coolsite/women/views.py
+++ djsite/coolsite/women/views.py
@@ -1,20 +1,27 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import *
+
+menu = ["about site", "Add Paragraph", "Feed Back", "enter"]
 
 def index(request):
-    return HttpResponse("Page app women")
+    posts = Women.objects.all()
+    return render(request, 'women/index.html',{'posts': posts, 'menu': menu, 'title':'Main Page'})
 
-def categories(request, catid):
-    if(request.GET):
-        print(request.GET)
-    return HttpResponse(f"<h1>Articles by categories</h1><p>{catid}</p>")
+def about(request):
+    return render(request, 'women/about.html',{'menu': menu, 'title':'Abour Site'})
 
-def archive(request, year):
-    if int(year) > 2020:
-        return redirect('home',permanent=True)
+# def categories(request, catid):
+#     if(request.GET):
+#         print(request.GET)
+#     return HttpResponse(f"<h1>Articles by categories</h1><p>{catid}</p>")
 
-    return HttpResponse(f"<h1>Articles by years</h1><p>{year}</p>")
+# def archive(request, year):
+#     if int(year) > 2020:
+#         return redirect('home',permanent=True)
+
+#     return HttpResponse(f"<h1>Articles by years</h1><p>{year}</p>")
 
 def pageNotFound(request,exception):
     return HttpResponseNotFound('<h1>Page Not Found, Search again</h1>')
--- djsite/coolsite/womenasd/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WomenasdConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'womenasd'
